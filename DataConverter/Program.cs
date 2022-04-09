84340aada79afb0d5d2b70ecd4819b7a64eef4d5
sa-borgia3 <sa.borgia3@gmail.com>
2022-04-09 17:57:25 +0200

    test and small fix

--- DataConverter/Program.cs
+++ DataConverter/Program.cs
@@ -4,6 +4,7 @@ using Json.Patch;
 using Json.Pointer;
 using OfficeOpenXml;
 using Json.More;
+using System;
 
 namespace DataConverter
 {
@@ -18,34 +19,38 @@ namespace DataConverter
             List<string[]> listRow = spreadsheet.ReadData();
 
             //list of dictionary [{}]
-            List<Dictionary<string, string>> listDict = new List<Dictionary<string, string>>();
-
+            List<Dictionary<string, JsonElement>> listDict = new List<Dictionary<string, JsonElement>>();
+            
             for (int row = 0; row < listRow.Count; row++)
             {
                 //from second row (first row is header)
                 if (row == 0) continue;
                 
-                Dictionary<string, string> dict = new Dictionary<string, string>();
+                Dictionary<string, JsonElement> dict = new Dictionary<string, JsonElement>();
+            
 
                 for (int col = 0; col < listRow[row].Length; col++)
                 {
-                    
-                    dict.Add(listRow[0][col],listRow[row][col]);
+                    //check and parse datatype
+
+                    dict.Add(listRow[0][col],listRow[row][col].AsJsonElement());
                 }
 
-                listDict.Add(dict);
-                
+                listDict.Add(dict);           
             }
-      
-            var json = JsonSerializer.Serialize(listDict);
 
+            //conversione json data
             
+
+            var json = JsonSerializer.Serialize(listDict);
+            
+                        
             //JsonPointer pointer = JsonPointer.Parse("/1/Nome");
             //string json = "[{\"Nome\": \"Salvatore\",\"Age\": 29}, {\"Nome\": \"Agostino\",\"Age\": 25}]";
             //JsonPatch patch = new JsonPatch(PatchOperation.Replace(pointer, "Pippo"));
             //JsonDocument result = patch.Apply(JsonDocument.Parse(json));
             //JsonElement jsonfinale = JsonSchema.ToJsonElement(result);
-            //Console.WriteLine(jsonfinale);
+            //Console.WriteLine(json);
 
         }
 
