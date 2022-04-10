91c995c58cd7be062ddb2eba209c739c16ed7b03
sa-borgia3 <sa.borgia3@gmail.com>
2022-04-10 13:01:39 +0200

    clean and tests

--- DataConverter/Program.cs
+++ DataConverter/Program.cs
@@ -14,43 +14,16 @@ namespace DataConverter
         {
 
 
-            Spreadsheet spreadsheet = new Spreadsheet("/Users/salvatoreborgia/Downloads/test.xlsx", "xlsx");
+            var spreadsheet = new Spreadsheet("/Users/salvatoreborgia/Downloads/test.xlsx", "xlsx");
+            spreadsheet.ReadData();
+            string json = spreadsheet.ConvertToJson();
 
-            List<string[]> listRow = spreadsheet.ReadData();
-
-            //list of dictionary [{}]
-            List<Dictionary<string, JsonElement>> listDict = new List<Dictionary<string, JsonElement>>();
-            
-            for (int row = 0; row < listRow.Count; row++)
-            {
-                //from second row (first row is header)
-                if (row == 0) continue;
-                
-                Dictionary<string, JsonElement> dict = new Dictionary<string, JsonElement>();
-            
-
-                for (int col = 0; col < listRow[row].Length; col++)
-                {
-                    //check and parse datatype
-
-                    dict.Add(listRow[0][col],listRow[row][col].AsJsonElement());
-                }
-
-                listDict.Add(dict);           
-            }
-
-            //conversione json data
-            
-
-            var json = JsonSerializer.Serialize(listDict);
-            
-                        
             //JsonPointer pointer = JsonPointer.Parse("/1/Nome");
             //string json = "[{\"Nome\": \"Salvatore\",\"Age\": 29}, {\"Nome\": \"Agostino\",\"Age\": 25}]";
             //JsonPatch patch = new JsonPatch(PatchOperation.Replace(pointer, "Pippo"));
             //JsonDocument result = patch.Apply(JsonDocument.Parse(json));
             //JsonElement jsonfinale = JsonSchema.ToJsonElement(result);
-            //Console.WriteLine(json);
+            Console.WriteLine(json);
 
         }
 
