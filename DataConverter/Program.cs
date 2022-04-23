99539abc98207a5230dca80976b791e65e0110a2
sa-borgia3 <sa.borgia3@gmail.com>
2022-04-23 21:58:14 +0200

    clean program

--- DataConverter/Program.cs
+++ DataConverter/Program.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Text.Json;
-using Json.Patch;
-using Json.Pointer;
-using OfficeOpenXml;
-using Json.More;
-using System;
+﻿using System;
 
 namespace DataConverter
 {
@@ -14,19 +8,13 @@ namespace DataConverter
         {
 
 
-            var spreadsheet = new Spreadsheet("/Users/salvatoreborgia/Downloads/test.xlsx", "xlsx");
+            var spreadsheet = new Spreadsheet("/Users/salvatoreborgia/Downloads/test2.xlsx", "xlsx");
 
             spreadsheet.ReadData();
 
             string json = spreadsheet.ConvertToJson();
 
-            //JsonPointer pointer = JsonPointer.Parse("/1/Nome");
-            //string json = "[{\"Nome\": \"Salvatore\",\"Age\": 29}, {\"Nome\": \"Agostino\",\"Age\": 25}]";
-            //JsonPatch patch = new JsonPatch(PatchOperation.Replace(pointer, "Pippo"));
-            //JsonDocument result = patch.Apply(JsonDocument.Parse(json));
-            //JsonElement jsonfinale = JsonSchema.ToJsonElement(result);
             Console.WriteLine(json);
-
 
         }
 
